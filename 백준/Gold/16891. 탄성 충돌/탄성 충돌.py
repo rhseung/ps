4d8332fb19bc3a9dd5530448aa9cc565183c8822
Rhseung <56152093+Rhseung@users.noreply.github.com>
2023-03-19 14:48:08 +0900

    [Gold II] Title: 탄성 충돌, Time: 324 ms, Memory: 134240 KB -BaekjoonHub

--- 백준/Gold/16891. 탄성 충돌/탄성 충돌.py
+++ 백준/Gold/16891. 탄성 충돌/탄성 충돌.py
@@ -10,38 +10,6 @@ def inputline(type, split=False):
 	else:
 		return type(stdin.readline().strip())
 
-class object:
-	def __init__(self, mass: int, velocity: Fraction):
-		self.m = mass
-		self.v = velocity
-
-def collision(object1: object, object2 : object):
-	v1 = object1.v
-	v2 = object2.v
-	object1.v = Fraction(
-		v1 * (object1.m - object2.m) + v2 * (2 * object2.m),
-		object1.m + object2.m
-	)
-	object2.v = Fraction(
-		v1 * (2 * object1.m) - v2 * (object1.m - object2.m),
-		object1.m + object2.m
-	)
-
 if __name__ == '__main__':
 	n = inputline(int)
-
-	object1 = object(1, Fraction(0))
-	object2 = object(n**2, Fraction(-1))
-
-	c = 0
-	while True:
-		if 0 <= object1.v <= object2.v:
-			break
-
-		if object1.v < 0 and object1.v <= object2.v:
-			object1.v *= -1
-		else:
-			collision(object1, object2)
-		c += 1
-
-	print(c)
+	print(floor(pi/atan(1/n) - 0.0001))
