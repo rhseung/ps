b4165a6fc68600cf83e6aef906329a32061de220
rhseung <rhseungg@gmail.com>
2023-04-02 12:25:55 +0900

    .

--- main.cpp
+++ main.cpp
@@ -7,43 +7,59 @@ using namespace std;
 using lnt = long long int;
 using uint = unsigned int;
 
-lnt phi(lnt n) {
-    lnt result = n;
+lnt power(lnt n, int p, int mod) {
+    if (p == 0) return 1;
+    else if (p == 1) return n;
 
-    for (int i = 2; i * i <= n; ++i) {
-        if (n % i == 0) {
-            while (n % i == 0) {
-                n /= i;
-            }
-            result -= result / i;
+    lnt x = (power(n, p / 2, mod) % mod) * (power(n, p / 2, mod) % mod) % mod;
+    if (p % 2 == 0) return x;
+    else return (x * n) % mod;
+}
+
+lnt combination(lnt n, lnt r, int mod) {
+    if (r == 0) return 1;
+    else if (n == r) return 1;
+    else {
+        lnt ret = 1;
+        for (lnt i = 1; i <= r; i++) {
+            ret = (ret * (n - i + 1)) % mod;
+            ret = (ret * power(i, mod - 2, mod)) % mod;
         }
+        return ret;
     }
+}
 
-    if (n > 1) {
-        result -= result / n;
+deque<lnt> seperate(lnt n, int p) {
+    deque<lnt> ret;
+    while (n > 0) {
+        ret.push_back(n % p);
+        n /= p;
     }
-
-    return result;
+    return ret;
 }
 
 int main() { setup;
-    lnt n;
-    cin >> n;
+    lnt n, k;
+    int m;
+    cin >> n >> k >> m;
 
-    if (n == 1) {
-        cout << 1 << endl;
-        return 0;
+    deque<lnt> n_s = seperate(n, m);
+    deque<lnt> k_s = seperate(k, m);
+
+    lnt ret = 1;
+
+    // n_s, k_s 출력
+//    cout << "n_s: ";
+//    for (auto i: n_s) cout << i << " ";
+//    cout << endl << "k_s: ";
+//    for (auto i: k_s) cout << i << " ";
+//    cout << endl;
+
+    for (int i = 0; i < min(n_s.size(), k_s.size()); ++i) {
+        ret = (ret % m * combination(n_s[i], k_s[i], m) % m) % m;
     }
 
-    for (lnt x = 2; x <= n; ++x) {
-        if (n % x == 0) {
-            if (x * phi(x) == n) {
-                cout << x << endl;
-                return 0;
-            }
-        }
-    }
+    cout << ret << endl;
 
-	cout << -1 << endl;
-	return 0;
+    return 0;
 }
--- main.py
+++ main.py
@@ -1,26 +1,48 @@
 from sys import stdin
+from functools import lru_cache
 from sys import setrecursionlimit
 from math import gcd
 from math import floor
 from math import sqrt
 from random import randint
 def inputline(type_, split=False):
-	if split:
+	if split == True:
 		return list(map(type_, stdin.readline().strip().split()))
 	else:
 		return type_(stdin.readline().strip())
 
+dp = [[0 for _ in range(2000)] for _ in range(2000)]
+
+def separate(n: int, p: int) -> list[int]:
+	ret = list[int]()
+	while n > 0:
+		ret.insert(0, n % p)
+		n //= p
+	return ret
+
+def combination(n: int, r: int, m: int) -> int:    
+	if r == 0 or n == r:
+		return 1
+	elif r == 1:
+		return n % m
+	elif dp[n][r] != 0:
+		return dp[n][r]
+	else:
+		dp[n][r] = (combination(n-1, r-1, m) % m + combination(n-1, r, m) % m) % m
+		return dp[n][r]
+
 if __name__ == '__main__':
-	N = inputline(int)
-	numbers = inputline(int, split=True)
-	numbers.sort()
-
-	if N == 1:
-		print(sum(numbers[0:5]))
-		exit()
-
-	a = [min(numbers[0], numbers[5]), min(numbers[1], numbers[4]), min(numbers[2], numbers[3])]
-	a1, a2, a3 = min(a[0], a[1], a[2]), min(a[0] + a[1], a[1] + a[2], a[2] + a[0]), a[0] + a[1] + a[2]
-	n1, n2, n3 = 4*(N-1)*(N-2)+(N-2)**2, 8*N-12, 4
-
-	print(a1*n1 + a2*n2 + a3*n3)
+	# m, n, p = inputline(int, split=True)
+	
+	# m_s = separate(m, p)
+	# n_s = separate(n, p)
+	
+	# ret = 1
+	# for i in range(min(len(m_s), len(n_s))):
+	# 	ret = (ret % p * combination(m_s[i], n_s[i], p) % p) % p
+  
+	# print(ret)
+ 
+	for i in range(1, 10):
+		for j in range(1, 10):
+			print(f"{i}C{j} = {combination(i, j, 1000000007)}")
