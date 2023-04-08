918e3cdb06952b1e09730f656dacb00c211a9efb
rhseung <rhseungg@gmail.com>
2023-04-08 09:12:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,44 +5,43 @@ from math import gcd
 from math import floor
 from math import sqrt
 from random import randint
-def inputline(type_, split=False):
+def inputstream(type_, split=False):
 	if split == True:
 		return list(map(type_, stdin.readline().strip().split()))
 	else:
 		return type_(stdin.readline().strip())
 
-dp = [[0 for _ in range(2000)] for _ in range(2000)]
-
-def separate(n: int, p: int) -> list[int]:
-	ret = list[int]()
-	while n > 0:
-		ret.insert(0, n % p)
-		n //= p
-	return ret
-
-def combination(n: int, r: int, m: int) -> int:    
-	if r == 0 or n == r:
+def phi(n):
+	if n == 1:
 		return 1
-	elif r == 1:
-		return n % m
-	elif dp[n][r] != 0:
-		return dp[n][r]
+		
+	result = n
+	for i in range(2, int(n ** 0.5) + 1):
+		if n % i == 0:
+			while n % i == 0:
+				n //= i
+			result -= result // i
+	if n > 1:
+		result -= result // n
+	return result
+
+def pow(n, p, mod):
+	if p == 0:
+		return 1
 	else:
-		dp[n][r] = (combination(n-1, r-1, m) % m + combination(n-1, r, m) % m) % m
-		return dp[n][r]
+		x = (pow(n, p // 2, mod) % mod * pow(n, p - p // 2, mod) % mod) % mod
+		if p % 2 == 1: x = (x * n) % mod
+		return x
+
+# f(n, mod) = n ** f(n, mod % φ(mod))
+def f(n, mod):
+    print(mod)
+    
+    if mod == 1 or n == 1: return 1
+    else: return pow(n, f(n, mod % phi(mod)) % mod, mod)
 
 if __name__ == '__main__':
-	# m, n, p = inputline(int, split=True)
-	
-	# m_s = separate(m, p)
-	# n_s = separate(n, p)
-	
-	# ret = 1
-	# for i in range(min(len(m_s), len(n_s))):
-	# 	ret = (ret % p * combination(m_s[i], n_s[i], p) % p) % p
-  
-	# print(ret)
- 
-	for i in range(1, 10):
-		for j in range(1, 10):
-			print(f"{i}C{j} = {combination(i, j, 1000000007)}")
+    T = inputstream(int)
+    for i in range(T):
+        n, mod = inputstream(int, split=True)
+        print(f(n, mod))
