b9e63f1d0305cd6d6dd5b786384d93ed9ddcbbb3
Hyunseung Ryu <rhseungg@gmail.com>
2023-10-08 21:57:49 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 636 ms, Memory: 213684 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -8,16 +8,17 @@ input = sys.stdin.readline
 query = input().strip()
 n = int(input().strip())
 
-counters = [Counter(query[0])]
-for i in range(1, len(query)):
-	c = counters[i - 1].copy()
-	c[query[i]] += 1
-	counters.append(c)
+counts = {}
+for c in 'abcdefghijklmnopqrstuvwxyz':
+	counts[c] = [0 if query[0] != c else 1]
 
-empty = Counter()
 for i in range(n):
 	alphabet, l, r = input().split()
 	l, r = int(l), int(r)
 	
-	count = counters[r] - (counters[l - 1] if l != 0 else empty)
-	print(count[alphabet])
+	if len(counts[alphabet]) == 1:
+		for j in range(1, len(query)):
+			counts[alphabet].append(counts[alphabet][j - 1] + (1 if query[j] == alphabet else 0))
+	
+	count = counts[alphabet][r] - (counts[alphabet][l - 1] if l != 0 else 0)
+	print(count)
