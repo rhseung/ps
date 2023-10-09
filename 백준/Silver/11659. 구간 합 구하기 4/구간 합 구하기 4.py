70d8fed606aeef17585a46011f850250f2db3fcb
Hyunseung Ryu <rhseungg@gmail.com>
2023-10-09 10:55:01 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 256 ms, Memory: 126224 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,14 +1,16 @@
 import sys
+
+problem_url = "https://boj.kr/11659"
+problem_name = "구간 합 구하기 4"
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-arr = list(map(int, input().split()))
+A = list(map(int, input().split()))
+C = [0 for _ in range(n + 1)]   # padding
 
-cum_arr = [arr[0]]
-for i in range(1, len(arr)):
-	cum_arr.append(cum_arr[i - 1] + arr[i])
+for i in range(1, n + 1):
+	C[i] = C[i - 1] + A[i - 1]
 
 for _ in range(m):
-	i, j = map(lambda x: int(x) - 1, input().split())
-	
-	print(cum_arr[j] - (0 if i - 1 == -1 else cum_arr[i - 1]))
+	x1, x2 = map(int, input().split())
+	print(C[x2] - C[x1 - 1])
