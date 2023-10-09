a71d5cb975386a340f29363f016a97dd69fb21e7
Hyunseung Ryu <rhseungg@gmail.com>
2023-10-09 10:20:02 +0900

    [Bronze IV] Title: 숫자의 합, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.py
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.py
@@ -1,5 +1,15 @@
 import sys
+
+problem_url = "https://boj.kr/11720"
+problem_name = "숫자의 합"
 input = sys.stdin.readline
 
 n = int(input())
-print(sum(map(int, input().strip())))
+pile = int(input())
+
+s = 0
+while pile > 0:
+	s += pile % 10
+	pile //= 10
+	
+print(s)
