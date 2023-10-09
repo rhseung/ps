18f3dbbe62479ac55e10403a6d0cf97fd3505647
Hyunseung Ryu <rhseungg@gmail.com>
2023-10-09 10:15:28 +0900

    [Bronze I] Title: 평균, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,8 +1,9 @@
 import sys
+
+problem_url = "https://boj.kr/1546"
+problem_name = "평균"
 input = sys.stdin.readline
 
 n = int(input())
-scores = list(map(int, input().split()))
-sum = sum(scores) / max(scores) * 100
-
-print(sum / n)
+A = list(map(int, input().split()))
+print((sum(A) / len(A)) / max(A) * 100)
