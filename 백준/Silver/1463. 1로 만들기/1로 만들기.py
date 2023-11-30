7f24e82e91bca66dc1b072c222fb4e6189bda82b
Hyunseung Ryu <rhseungg@gmail.com>
2023-11-30 09:39:28 +0900

    [Silver III] Title: 1로 만들기, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -8,32 +8,20 @@ input = sys.stdin.readline
 n = int(input())
 
 # 10 -> 9 -> 3 -> 1 로 하는 것이 제일 빠르기 때문에 그리디로는 풀 수 없음
-dp = [0] * (n + 1)
+dp = {1: 0}
 
-# note: top-down은 재귀 호출 limit에 걸리게 되는데 setrecursionlimit를 늘리면 메모리가 늘어나서 메모리초과
-# def solve(n):
-#     if n not in dp:
-#         min_way = inf
-#
-#         if n % 3 == 0:
-#             min_way = min(min_way, solve(n // 3) + 1)
-#         if n % 2 == 0:
-#             min_way = min(min_way, solve(n // 2) + 1)
-#         min_way = min(min_way, solve(n - 1) + 1)
-#
-#         dp[n] = min_way
-#
-#     return dp[n]
+def get(n):
+    if n in dp:
+        return dp[n]
+    if n % 6 == 0:
+        dp[n] = min(get(n // 3), get(n // 2)) + 1
+    elif n % 3 == 0:
+        dp[n] = min(get(n // 3), get(n - 1)) + 1
+    elif n % 2 == 0:
+        dp[n] = min(get(n // 2), get(n - 1)) + 1
+    else:
+        dp[n] = get(n - 1) + 1
 
-for i in range(2, n+1):
-    min_way = inf
+    return dp[n]
 
-    if i % 3 == 0:
-        min_way = min(min_way, dp[i // 3] + 1)
-    if i % 2 == 0:
-        min_way = min(min_way, dp[i // 2] + 1)
-    min_way = min(min_way, dp[i - 1] + 1)
-
-    dp[i] = min_way
-
-print(dp[n])
+print(get(n))
