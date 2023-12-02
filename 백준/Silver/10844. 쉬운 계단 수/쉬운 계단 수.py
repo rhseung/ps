06c380a99c20a85c65f0cd02211d894b2ad5a148
Hyunseung Ryu <rhseungg@gmail.com>
2023-12-03 00:20:34 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,22 +1,14 @@
-__problem__ = 'https://boj.kr/10844', '쉬운 계단 수'
-
-import sys
-
-input = sys.stdin.readline
-
 n = int(input())
+D = [[0] * 10 for _ in range(n+1)]
+D[1][1:10] = [1] * 9
 
-# 0    0    1    2    3    4    5    6    7    8    9    0  -> 12개
-#          0 2  1 3  2 4  3 5  4 6  5 7  6 8  7 9  8
-# 두 번째 자리 부터는 0이 올 수 있기 때문에 왼쪽 오른쪽 패딩은 별개로 하고 0~9도 써야함
+for i in range(2, n+1):
+    for j in range(0, 9+1):
+        if j == 0:
+            D[i][j] = D[i-1][j+1]
+        elif j == 9:
+            D[i][j] = D[i-1][j-1]
+        else:
+            D[i][j] = (D[i-1][j-1] + D[i-1][j+1]) % 1000000000
 
-D = [[0]*12 for _ in range(n)]
-D[0][2:11] = [1] * 9    # base case
-
-for i in range(1, n):
-    for j in range(1, 11):
-        # 9는 0과 10으로 계단 관계이나 10은 안되므로 조건 분기를 해줘도 되나,
-        # 그냥 양쪽에 0을 padding하여 D[i-1][j+1] = 0으로 합을 무시할 수 있게 함
-        D[i][j] = D[i - 1][j - 1] + D[i - 1][j + 1]
-
-print(sum(D[n - 1]) % 1_000_000_000)
+print(sum(D[n]) % 1000000000)
