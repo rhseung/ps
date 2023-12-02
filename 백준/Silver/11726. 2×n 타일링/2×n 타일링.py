dff490320400ba35cf025f361e50cb5a0f466b8f
Hyunseung Ryu <rhseungg@gmail.com>
2023-12-02 23:19:29 +0900

    [Silver III] Title: 2×n 타일링, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,18 +1,7 @@
-__problem__ = 'https://boj.kr/11726', '2×n 타일링'
-
-import sys
-
-input = sys.stdin.readline
-
 n = int(input())
-m = 10007
+D = {1: 1, 2: 2}
 
-D = [0] * n
-D[0] = 1
-if n > 1:
-    D[1] = 2    # n = 1일 때 index error 남
+for i in range(3, n + 1):
+    D[i] = (D[i - 1] + D[i - 2]) % 10007
 
-for i in range(2, n):
-    D[i] = (D[i - 1] + D[i - 2]) % m
-
-print(D[n - 1])
+print(D[n])
