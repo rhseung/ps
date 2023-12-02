932c353a9c4006cd28919e05a35dc77e71541230
Hyunseung Ryu <rhseungg@gmail.com>
2023-12-02 22:52:55 +0900

    [Silver III] Title: 퇴사, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,15 +1,15 @@
 N = int(input())
-T = [0] * (N + 1)
-P = [0] * (N + 1)
-D = [0] * (N + 2)
+T = [0] * N
+P = [0] * N
+D = [0] * (N + 1)
 
-for i in range(1, N + 1):
+for i in range(0, N):
     T[i], P[i] = map(int, input().split())
 
-for i in range(N, 0, -1):
-    if i + T[i] <= N + 1:
+for i in range(N - 1, -1, -1):
+    if i + T[i] <= N:
         D[i] = max(D[i + 1], D[i + T[i]] + P[i])   # 상담 스킵 or 상담 선택
     else:
         D[i] = D[i + 1]     # 상담 스킵
 
-print(D[1])
+print(D[0])
