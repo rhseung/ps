b6708e1b31c9d51ff2d784a92ed6e44499a42ae5
Hyunseung Ryu <rhseungg@gmail.com>
2023-12-02 22:15:08 +0900

    [Silver III] Title: 1로 만들기, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,27 +1,17 @@
-__problem__ = 'https://boj.kr/1463', '1로 만들기'
-
-import sys
-from math import inf
-
-input = sys.stdin.readline
-
 n = int(input())
-
-# 10 -> 9 -> 3 -> 1 로 하는 것이 제일 빠르기 때문에 그리디로는 풀 수 없음
-dp = {1: 0}
+D = {1: 0}
 
 def get(n):
-    if n in dp:
-        return dp[n]
-    if n % 6 == 0:
-        dp[n] = min(get(n // 3), get(n // 2)) + 1
-    elif n % 3 == 0:
-        dp[n] = min(get(n // 3), get(n - 1)) + 1
-    elif n % 2 == 0:
-        dp[n] = min(get(n // 2), get(n - 1)) + 1
-    else:
-        dp[n] = get(n - 1) + 1
-
-    return dp[n]
+    if n not in D:
+        if n % 6 == 0:
+            D[n] = min(get(n // 2), get(n // 3)) + 1
+        elif n % 2 == 0:
+            D[n] = min(get(n - 1), get(n // 2)) + 1
+        elif n % 3 == 0:
+            D[n] = min(get(n - 1), get(n // 3)) + 1
+        else:
+            D[n] = get(n - 1) + 1
+    
+    return D[n]
 
 print(get(n))
