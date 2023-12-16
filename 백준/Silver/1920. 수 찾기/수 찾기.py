4b63a44c86212ee352ccbfe70104a83a01fb8cbf
Hyunseung Ryu <rhseungg@gmail.com>
2023-12-17 00:42:39 +0900

    [Silver IV] Title: 수 찾기, Time: 220 ms, Memory: 136428 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,27 +1,21 @@
-__problem__ = 'https://boj.kr/1920', '수 찾기'
-
-import sys
-
-input = sys.stdin.readline
-
 n = int(input())
 A = list(map(int, input().split()))
 m = int(input())
-K = list(map(int, input().split()))
+X = list(map(int, input().split()))
 
 A.sort()
 
-for key in K:
+for x in X:
     flag = False
+    left, right = 0, n - 1
 
-    i, j = 0, n - 1
-    while i <= j:
-        mid = (i + j) // 2
+    while left <= right:
+        mid = (left + right) // 2
 
-        if A[mid] > key:
-            j = mid - 1
-        elif A[mid] < key:
-            i = mid + 1
+        if x < A[mid]:
+            right = mid - 1
+        elif A[mid] < x:
+            left = mid + 1
         else:
             flag = True
             break
