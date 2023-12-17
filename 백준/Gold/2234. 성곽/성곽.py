1657160af7940edeae47544e85ffc82ffa48e060
Hyunseung Ryu <rhseungg@gmail.com>
2023-12-17 14:33:20 +0900

    [Gold III] Title: 성곽, Time: 336 ms, Memory: 116224 KB -BaekjoonHub

--- 백준/Gold/2234. 성곽/성곽.py
+++ 백준/Gold/2234. 성곽/성곽.py
@@ -1,70 +1,61 @@
-__problem__ = 'https://boj.kr/2234', '성곽'
-
-import sys
 from collections import deque
 
-input = sys.stdin.readline
-pos = tuple[int, int]
+n, m = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(m)]     # 이거 왜 n*m이 아니고 m*n 이냐
+V = [[False] * n for _ in range(m)]
 
-col, row = map(int, input().split())
-castle = [list(map(int, input().split())) for _ in range(row)]
-
-visited = set()
-room_count = 0
-max_room = 0
-
-def bfs(now: pos) -> int:
-    queue = deque([now])
-    visited.add(now)
-    room_size = 1
+def bfs(v: tuple[int, int]):
+    queue = deque([v])
+    V[v[0]][v[1]] = True
+    size = 1
 
     while queue:
         i, j = queue.popleft()
-        v = castle[i][j]
+        num = A[i][j]
 
         ways = []
-        if not v & 1:   # west
+        if not num & 1:
             ways.append((i, j - 1))
-        if not v & 2:   # north
+        if not num & 2:
             ways.append((i - 1, j))
-        if not v & 4:   # east
+        if not num & 4:
             ways.append((i, j + 1))
-        if not v & 8:   # south
+        if not num & 8:
             ways.append((i + 1, j))
 
-        for r, c in ways:
-            if row > r >= 0 and col > c >= 0:     # 벽 부수고 이동할 때 적절한 위치인지 확인 ex. (0, 0) -> 왼쪽 벽 부수고 (0, -1)
-                if (r, c) not in visited:
-                    queue.append((r, c))
-                    visited.add((r, c))
-                    room_size += 1
+        for a, b in ways:
+            if 0 <= a < m and 0 <= b < n:
+                if not V[a][b]:
+                    V[a][b] = True
+                    queue.append((a, b))
+                    size += 1
 
-    return room_size
+    return size
 
-for i in range(row):
-    for j in range(col):
-        if (i, j) not in visited:
-            room_count += 1
-            max_room = max(max_room, bfs((i, j)))
+cnt = 0
+max_size = 0
+for i in range(m):
+    for j in range(n):
+        if not V[i][j]:
+            max_size = max(max_size, bfs((i, j)))
+            cnt += 1
 
-print(room_count)
-print(max_room)
+print(cnt)
+print(max_size)
 
-# 가장 큰 사이즈의 방이 벽 하나를 두고 옆에 안 붙어있을 수도 있음
-# room_sizes.sort()
-# print(room_sizes[-1] + room_sizes[-2])
+V = [[False] * n for _ in range(m)]     # note: 처음에 리셋 해주는 거 까먹지 말기
+max_size = 0
+for i in range(m):
+    for j in range(n):
+        if not V[i][j]:
+            c = 1
+            while A[i][j] >= c:
+                A[i][j] -= c
+                # 다시 같은 방을 순회하기 위해 visited를 초기화하는데,
+                # 다른 방들은 이미 돌고 for 문은 지나왔기에 다른 것도 다 초기화해도 알 빠 아님
+                V = [[False] * n for _ in range(m)]
+                max_size = max(max_size, bfs((i, j)))
+                A[i][j] += c
+                c *= 2
 
-visited = set()
-max_room = 0
-
-for i in range(row):
-    for j in range(col):
-        if (i, j) not in visited:
-            for wall in (1, 2, 4, 8):
-                if castle[i][j] >= wall:
-                    visited = set()
-                    castle[i][j] -= wall
-                    max_room = max(max_room, bfs((i, j)))
-                    castle[i][j] += wall
-
-print(max_room)
+print(max_size)
