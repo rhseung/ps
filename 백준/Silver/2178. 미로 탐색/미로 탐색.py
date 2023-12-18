84f179d6ba36f4591b58f4e57eacdd45e49789cf
Hyunseung Ryu <rhseungg@gmail.com>
2023-12-18 20:41:41 +0900

    [Silver I] Title: 미로 탐색, Time: 156 ms, Memory: 114144 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,29 +1,23 @@
-__problem__ = 'https://boj.kr/2178', '미로 탐색'
-
-import sys
 from collections import deque
 
-input = sys.stdin.readline
-
 n, m = map(int, input().split())
-maze = [list(map(int, list(input().strip()))) for _ in range(n)]
+A = [list(map(int, list(input().strip()))) for _ in range(n)]
 
-def bfs(g, s: tuple[int, int]):
+def bfs(i, j):
     visited = [[False] * m for _ in range(n)]
+    visited[i][j] = True
 
-    visited[s[0]][s[1]] = True
-    q = deque([s])
+    queue = deque([(i, j)])
+    while queue:
+        a, b = queue.popleft()
 
-    while q:
-        a, b = q.popleft()
+        for di, dj in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+            if 0 <= a + di < n and 0 <= b + dj < m:
+                if A[a + di][b + dj] > 0:
+                    if not visited[a + di][b + dj]:
+                        visited[a + di][b + dj] = True
+                        queue.append((a + di, b + dj))
+                        A[a + di][b + dj] = A[a][b] + 1
 
-        for x, y in [(a-1, b), (a+1, b), (a, b-1), (a, b+1)]:
-            if 0 <= x < n and 0 <= y < m:
-                if g[x][y] != 0:
-                    if not visited[x][y]:
-                        visited[x][y] = True
-                        g[x][y] = g[a][b] + 1
-                        q.append((x, y))
-
-bfs(maze, (0, 0))
-print(maze[n-1][m-1])
+bfs(0, 0)
+print(A[n - 1][m - 1])
