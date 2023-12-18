274a4ff1f3e993b5b62a3f58e965349755818c5a
Hyunseung Ryu <rhseungg@gmail.com>
2023-12-18 22:46:47 +0900

    [Gold IV] Title: 수 묶기, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.py
+++ 백준/Gold/1744. 수 묶기/수 묶기.py
@@ -1,46 +1,34 @@
-__problem__ = 'https://boj.kr/1744', '수 묶기'
-
-import sys
-
-input = sys.stdin.readline
-
-# 0은 음수랑 곱해서 소멸시키기
-#  - [-5 -4 0] 에서 0은 오히려 곱하는게 안 좋음. 0은 모든 음수들을 다 곱하게 둔 뒤 홀수 개라 음수가 하나 남으면 그 때 0과 곱해주기
-# 1은 그냥 더해. 1*n = n < 1+n 이기 때문.
-# 나머지는 (최대*최대-1) 로?
-
-n = int(input().strip())
+n = int(input())
 pos = []
 neg = []
-zero = False
+has_zero = False
+
 s = 0
+for i in range(n):
+    x = int(input())
 
-for _ in range(n):
-    tmp = int(input().strip())
-
-    if tmp > 0:
-        if tmp == 1:
-            s += 1
-        else:
-            pos.append(tmp)
-    elif tmp < 0:
-        neg.append(-tmp)
+    if x == 1:
+        s += x
+    elif x == 0:
+        has_zero = True
+    elif x > 0:
+        pos.append(x)
     else:
-        zero = True
+        neg.append(-x)  # note: 양수로 넣지 않는다면 sort 시 [-1, -2, -3, ...] 가 아니라 [..., -3, -2, -1] 가 됨
 
 pos.sort()
 neg.sort()
 
-while len(pos) > 1:
+while len(pos) >= 2:
     s += pos.pop() * pos.pop()
+
+while len(neg) >= 2:
+    s += neg.pop() * neg.pop()
 
 if pos:
     s += pos.pop()
 
-while len(neg) > 1:
-    s += neg.pop() * neg.pop()
-
-if neg and not zero:
-    s += -neg.pop()     # neg는 양수로 저장되어있음 주의
+if neg and not has_zero:
+    s += -neg.pop()
 
 print(s)
