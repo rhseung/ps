b02549bf926c4352ec0a597f14cf2667ff341ae0
Hyunseung Ryu <rhseungg@gmail.com>
2024-05-16 14:41:39 +0900

    [Silver III] Title: 주유소, Time: 168 ms, Memory: 136396 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,18 +1,22 @@
 n = int(input().strip())
-dist = [0] + list(map(int, input().split()))
+dist = list(map(int, input().split()))
 prices = list(map(int, input().split()))
 
-oil = 0
 price = 0
+M = []
+cursor = 0
+val = 0
 
 for i in range(n - 1):
-    if oil < dist[i + 1]:
-        if i + 2 < n and prices[i] < prices[i + 1]:
-            price += prices[i] * (dist[i + 1] + dist[i + 2] - oil)
-            oil = dist[i + 1] + dist[i + 2]
-        else:
-            price += prices[i] * (dist[i + 1] - oil)
-            oil = dist[i + 1]
-    oil -= dist[i + 1]
+    if prices[cursor] > prices[i]:
+        price += prices[cursor] * val
+        # M.append((cursor, val))
+        cursor = i
+        val = dist[i]
+    else:
+        val += dist[i] 
+# M.append((cursor, val))
+price += prices[cursor] * val
 
 print(price)
+# print(M)
