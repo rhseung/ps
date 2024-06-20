8605703447515c0222b0c1a23046466938bcf326
Hyunseung Ryu <rhseungg@gmail.com>
2024-06-20 17:40:45 +0900

    [Silver III] Title: 정수 a를 k로 만들기, Time: 96 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.py
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.py
@@ -1,18 +1,12 @@
-import sys
-inf = sys.maxsize
-
 a, k = map(int, input().split())
 
-def min_operation(a: int, k: int) -> int:
-    D = [inf] * (k + 1)
-    D[a] = 0
-    
-    for i in range(a + 1, k + 1):
-        if i % 2 != 0:
-            D[i] = D[i - 1] + 1
-        else:
-            D[i] = min(D[i - 1], D[i // 2]) + 1
-    
-    return D[k]
+cnt = 0
+now = k
+while now != a:
+    cnt += 1
+    if now % 2 == 0 and now // 2 >= a:
+        now //= 2
+    else:
+        now -= 1
 
-print(min_operation(a, k))
+print(cnt)
