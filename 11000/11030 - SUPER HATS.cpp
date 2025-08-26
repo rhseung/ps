671909b695f633405d96dc60bb522941c37a67a6
Hyunseung Ryu <rhseungg@gmail.com>
2025-08-26 19:25:53 +0900

    solve: 11030 - SUPER HATS

---
+++ 11000/11030 - SUPER HATS.cpp
@@ -0,0 +1,79 @@
+// BOJ 11030 - SUPER HATS
+#include <bits/stdc++.h>
+#define endl "\n"
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+/**
+ * 유클리드 호제법
+ * @param a
+ * @param b
+ * @return gcd(a, b)
+ */
+inline ll gcd(const ll a, const ll b) {
+    if (b == 0)
+        return a;
+
+    return gcd(b, a % b);
+}
+
+ll mod_nonzero(const ll a, const ll b) {
+    return a > b ? a % b + b : a;
+}
+
+ll modpow(ll a, ll b, ll c) {
+    ll ret = 1;
+    while (b) {
+        if (b & 1) ret = mod_nonzero(ret * a, c);
+        b >>= 1;
+        a = mod_nonzero(a * a, c);
+    }
+    return ret;
+}
+
+ll phi(ll n) {
+    ll result = n;
+
+    for (ll i = 2; i * i <= n; ++i) {
+        if (n % i == 0) {
+            result -= result / i;
+            while (n % i == 0) n /= i;
+        }
+    }
+
+    if (n > 1)
+        result -= result / n;
+
+    return result;
+}
+
+ll f(const ll b, const ll x, const ll m) {
+    if (x == 0) return 1;
+
+    // a^p = a^(p mod phi(m)) mod m을 이용해 구할 수 있다.
+    const ll p = phi(m);
+    return modpow(b, f(b, x - 1, p), m);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    constexpr ll m = 100'000'000;
+
+    ll a, b;
+    cin >> a >> b;
+
+    const ll result = f(a, b, m);
+    if (result < m) {
+        cout << result << endl;
+    }
+    else {
+        cout << setw(8) << setfill('0') << result % m << endl;
+    }
+
+    return 0;
+}
