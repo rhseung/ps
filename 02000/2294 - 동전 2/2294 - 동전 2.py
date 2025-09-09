cf0f60e6d5c8c7c71fe9c0b72a8289934853b6cb
Hyunseung Ryu <rhseungg@gmail.com>
2025-09-09 12:39:31 +0900

    solve: 2294 - 동전 2

--- 02000/2294 - 동전 2/2294 - 동전 2.py
+++ 02000/2294 - 동전 2/2294 - 동전 2.py
@@ -1,17 +1,19 @@
 # BOJ 2294 - 동전 2
 import sys
 input = sys.stdin.readline
+inf = float('inf')
 
 def main():
     n, k = map(int, input().split())
     V = [int(input()) for _ in range(n)]
-    DP = [0] * (k + 1)
+    DP = [inf] * (k + 1)
+    DP[0] = 0
 
     for x in range(0, k + 1):
-        DP[x] = min(DP[k - V[i]] + 1 for i in range(n) if k >= V[i])
+        DP[x] = min((DP[x - V[i]] + 1 for i in range(n) if x >= V[i]), default=DP[x])
 
-    print(DP)
-    print(DP[k])
+    # print(DP)
+    print(-1 if DP[k] == inf else DP[k])
 
 if __name__ == "__main__":
     main()
