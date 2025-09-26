40cb954d619fa753b6f851d2d0291007ebbc1d6d
Hyunseung Ryu <rhseungg@gmail.com>
2025-09-26 14:27:09 +0900

    chore: edit and apply .gitignore

--- solving/1890 - 점프/1890 - 점프.py
+++
@@ -1,5 +0,0 @@
-# BOJ 1890 - 점프
-import sys
-input = sys.stdin.readline
-
-data = input().split()
--- solving/32240 - 비로소 서로소/32240 - 비로소 서로소.py
+++
@@ -1,5 +0,0 @@
-# BOJ 32240 - 비로소 서로소
-import sys
-input = sys.stdin.readline
-
-data = input().split()
