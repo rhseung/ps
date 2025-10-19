3bcbf934ce7d376ecd22a6589a29adfaddd42051
Hyunseung Ryu <rhseungg@gmail.com>
2025-10-19 15:08:48 +0900

    solve: 2747 - 피보나치 수

---
+++ 02000/2747 - 피보나치 수/2747 - 피보나치 수.py
@@ -0,0 +1,14 @@
+# BOJ 2747 - 피보나치 수
+import sys
+def input() -> str: return sys.stdin.readline()
+
+n = int(input())
+
+DP = [0] * (n + 1)
+if n + 1 > 1:
+    DP[1] = 1
+
+for i in range(2, n + 1):
+    DP[i] = DP[i - 1] + DP[i - 2]
+
+print(DP[n])
